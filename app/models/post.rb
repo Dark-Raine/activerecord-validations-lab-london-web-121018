256bfb01426f0ece224492119125aa23e3b16075
Dark-Raine <anderson-tomas@hotmail.com>
2019-01-15 15:08:08 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction)}
+    validates :title_check, absence: { message: "DIS IS CLICKBAIT" }
+    # validates :name, uniqueness: true
+
+    def title_check
+       
+
+    if self.title
+        
+        if !self.title.include?  "Believe" || "Won't" || "Secret"
+            true
+        else
+            false
+        end
+    else
+        false
+    end
+    end
 end
